6607bb00ba4cbd2c1e749a707d60ebfb7feea2cc
gokulcat <75694391+gokulcat@users.noreply.github.com>
2024-11-29 19:27:55 +0530

    Minor Changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,9 @@
 const jwt = require("jsonwebtoken");
 
+
+const cors = require('cors');
+app.use(cors());
+
 const authenticate = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Unauthorized" });
